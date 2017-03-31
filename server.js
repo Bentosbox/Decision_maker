fccd73c090486f5ff9c91f4739c317383405b406
Ben Li <ben_li5@yahoo.ca>
2017-03-31 01:18:03 +0000

    knex insert to databas

--- server.js
+++ server.js
@@ -45,44 +45,139 @@ app.use("/api/users", usersRoutes(knex));
 
 
 
-////////// Home page
+//////////////// GET PAGES ///////////////
+
 app.get("/", (req, res) => {
-  res.render("index");
+  res.redirect("/polls");
+});
+
+app.get("/polls", (req, res) => {
+  res.status(200).render("index")
+});
+
+app.get("/polls/admin/:id", (req, res) => {
+  res.status(200).render("result")
+});
+
+app.get("/polls/:id", (req, res) => {
+  res.status(200).render("vote")
 });
 
 
-///////// PLACEHOLDER FOR FULL MAILGUN RUN FUNCTION
 
-// app.post('/polls', (req, res) => {
-// }
+/////////////POST REQUESTS//////////////////
 
-function sendEmail() {
+app.post('/polls', (req, res) => {
+  let email_subject = req.body[""];
+  let email_text = req.body[""];
+  let email_admin = req.body[""];
+  /// may need for each to loop thorugh each voter///
+  let email_voter = req.body[""];
+  let time = req.body[""];
+  let admin_url = generateRandomString();
+  let voter_url = generateRandomString();
+  let url_voter = 'localhost8080:' + voters.url
+  let url_admin = 'localhost8080:' + decisions.admin_url
 
-  var data = {
-    from: 'Decision Maker <postmaster@sandbox0229991348f842509ff15dab0913c399.mailgun.org>',
-    to: 'ben_li5@yahoo.ca',
-    subject: 'Hello',
-    text: 'Testing some Mailgun awesomness!'
-  };
+  /////////GENERATE RANDOM STRING
+
+    //////////////// INSERT INFORMATION INTO TABLES ///////////////////
 
 
-  /////PlaceHolder For Actual Links
+  //////INSERT INTO DECISIONS TABLE///////
 
-  // var voterEmail = {
-  //   from: 'decisions.admin_email <postmaster@sandbox0229991348f842509ff15dab0913c399.mailgun.org>',
-  //   to: 'voters.email'
-  //   subject: 'decision.title'
-  //   text: 'decision.message'
-  // }
-  // mailgun.messages().send(data, function (error, body) {
-  //   console.log(body);
-  // }
+  knex('decisions')
+    .insert({title: email_subject, time: req.body[""], message: email_text, admin_email: email_admin, admin_name: req.body[""], admin_url: admin_url})
+    .returning('id')
+    .then(function(decisionId) {
+      // callback(decisionId[0]);
+      knex('voters')
+        .insert({email: , name: req.body[""], url: voter_url , decision_id: decisionId[0]})
+        .then(function(result) {
+      });
+    });
 
-  mailgun.messages().send(data, function (error, body) {
-    console.log(body);
-  });
+
+  //////INSERT INTO VOTERS TABLE//////////
+
+  knex('voters')
+    .insert({email: , name: req.body[""], url: voter_url , decision_id: })
+    .then(function(result) {
+    });
+
+  //////INSERT INTO OPTIONS TABLE//////////
+
+  knex('options')
+    .insert({title: req.body[""] description: req.body[""], decision_id: decisionId, total_rank:})
+    .then(function(result) {
+    });
+
+  knex('polls')
+    .insert({voter_id: decisionId, voter_id: , base_rank:})
+
+
+  ///////// PLACEHOLDER FOR FULL MAILGUN RUN FUNCTION
+    //////////PlaceHolder For Email Function////////////
+  function sendEmail() {
+    var data = {
+      from: 'Decision Maker <postmaster@sandbox0229991348f842509ff15dab0913c399.mailgun.org>',
+      to: 'ben_li5@yahoo.ca',
+      subject: 'Hello',
+      text: 'Testing some Mailgun awesomness!'
+    };
+    mailgun.messages().send(data, function (error, body) {
+      console.log(body);
+    });
+  }
+  sendEmail();
+  //////////////////////////////////////////////////////
+
+
+  ///VOTER EMAIL///
+
+    var voterEmail = {
+      from: 'Decision Maker <postmaster@sandbox0229991348f842509ff15dab0913c399.mailgun.org>',
+      to: voters.email
+      subject: email_subject
+      text: email_text, url_voter
+    }
+    mailgun.messages().send(voterEmail, function (error, body) {
+      console.log(body);
+    }
+
+  // /ADMIN EMAIL///
+
+  var adminEmail = {
+      from: 'Decision Maker <postmaster@sandbox0229991348f842509ff15dab0913c399.mailgun.org>',
+      to: decisions.admin_email
+      subject: email_subject
+      text: 'Thank you for using Decision Maker. Your administration and user link are as follows: ', url_admin, url_voter
+    }
+    mailgun.messages().send(adminEmail, function (error, body) {
+      console.log(body);
+    }
 }
-sendEmail();
+
+
+app.post('/polls/:id', (req, res) => {
+}
+
+/////////////////FUNCTIONS//////////////////
+
+
+// function to generate a random string of 6 alpha numeric characters
+function generateRandomString() {
+  var charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var text = "";
+  for( var i=0; i < 6; i++ ) {
+    text += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+  return text;
+}
+// Used a do-while loop to generate random string until it does not exist in the database
+//     do {
+//       var shortURL = generateRandomString();
+//     } while (urlDatabase[shortURL]);
 
 
 
