f6bcf7259368b816a24681e9e3be53164e5766c2
Vinay Balaji <vinaybalaji@gmail.com>
2017-04-02 22:00:57 +0000

    Changes to make vote.ejs functionality work

--- public/scripts/poll-vote.js
+++ public/scripts/poll-vote.js
@@ -129,11 +129,87 @@
   // });
 
   $( () => {
-    console.log("this is the ajax voter url request: " + voterURL);
+
     $.ajax({
       method: 'GET',
       url: '/' + voterURL + '/json'
     }).done(function (voteChoices) {
-      console.log(voteChoices[0]);
+      var voteArray = [];
+      console.log('Connection to server via ajax Get reuqest was successful');
+      for (var i=0; i<voteChoices.length; i++) {
+        console.log(voteChoices[i]);
+        voteArray.push(voteChoices[i]);
+        }
+
+      console.log('voteArray is: ' + voteArray);
+
+
+      $( "#sortable" ).sortable();
+      $( "#sortable" ).disableSelection();
+
+      var decisionObject = voteArray[0];
+
+      $('.decision-admin-name-display').text(decisionObject.admin_name);
+      $('.decision-admin-email-display').text(decisionObject.admin_email);
+      $('.decision-title-display').text(decisionObject.decision_title);
+      $('.decision-message-display').text(decisionObject.message);
+
+        //Need to call a function from Ellen's JS files that will pass this value (which is current time (in seconds) - poll created time (in seconds) + poll length time (in seconds))
+            // var timeLifeInSeconds = Math.round(Date.now()/1000) - decisionObject.time;
+
+        //Render the relevant information from the decisionObject object in the browser (check if Ellen can help)
+
+        var optionWrapper = $('#sortable');
+
+        for (var i = 0; i < voteArray.length; i++) {
+          optionWrapper.append('<li class="list-group-item option-title-display" id="' + voteArray[i].id + '"><span class="badge option-description-display">' + voteArray[i].description + '</span>' + voteArray[i].title + '</li></div>');
+        }
+
+        $('.submit-vote-button').on('click', function (event) {
+
+          event.preventDefault();
+
+          //Create Poll Objects based on how many options there are for this decision
+          var pollObjectArray = [];
+          var optionsList = document.getElementsByClassName('option-title-display');
+
+          for (var i = 0; i < optionsList.length; i++) {
+            var pollObject = {
+              decision_id: decisionObject.decision_id,
+              option_id: Number(optionsList[i].id),
+              rank: optionsList.length - i,
+              admin_url: decisionObject.admin_url
+            };
+            pollObjectArray.push(pollObject);
+          }
+
+          //Console logging the poll object for testing
+          console.log(pollObjectArray.length);
+          console.log(pollObjectArray[0]);
+
+          //Send pollObjectArray to SERVER --- Discuss with Ben
+
+          // $.ajax({
+          //   url: '/polls/' + voter_url,
+          //   method: 'POST',
+          //   data: pollObjectArray // sending pollObjectArray to SERVER
+          // }).done(
+          //   console.log('ajax call for posting to /polls/' + voter_url + ' is a success'); // check if any code needs to be executed when ajax post is done
+          // }).fail(function (err) {
+          //   console.log('failed');
+          // });
+
+
+          // var optionsList = document.getElementsByClassName('option-title-display');
+          // for (var i = 0; i < optionsList.length; i++) {
+          //   console.log(optionsList[i].innerText); // can render the option_title and option description_text using text
+          //   console.log(optionsList[i].id); // can get option_id value by setting the css id as the option_id value
+          //   optionsList[i].innerText = 'Sample' + i; // this can set the value of Span tag
+          // }
+        });
+
+
+
     });
+
   });
